1deab058cfbe10ad4322e4aa1f6084cd6b2a8c5e
Saleh Yassin <yass0016@gmail.com>
2019-03-01 22:43:12 -0500

    Converting to SQLite

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 
-import medications from "./medications";
 import data from "./data";
-import notifications from "./notifications";
 import settings from "./settings";
 
 const reducers = combineReducers({
   dataState: data,
-  medState: medications,
-  notificationsState: notifications,
   settingsState: settings
 });
 
